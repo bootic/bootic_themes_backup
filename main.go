017c644fac97e57a935e9ffe7a565464fdff5aaa
Ismael Celis <ismaelct@gmail.com>
2013-12-29 15:30:19 +0000

    Backup shop theme templates and assets to files.
    usage
    
    BOOTIC_ACCESS_TOKEN=xxx go run main.go

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+  "log"
+  "strings"
+  "net/http"
+  "errors"
+  "encoding/json"
+  "os"
+  "io/ioutil"
+)
+
+const API_URL = "https://api.bootic.net/v1"
+
+type AssetOrTemplate struct {
+  Class []string
+  Properties map[string]string
+  Links map[string]map[string]string
+}
+type Entity struct {
+  Class []string
+  Properties map[string]string
+  Entities map[string][]AssetOrTemplate
+  Links map[string]map[string]string
+}
+
+type ThemeRequest struct {
+  ShopId string
+  token string
+  Data *Entity
+  conn *http.Client
+}
+
+func (req * ThemeRequest) Get () error {
+  segments := []string{API_URL, "shops", req.ShopId, "theme.json"}
+  url := strings.Join(segments, "/")
+  log.Println("getting", url)
+
+  request, _ := http.NewRequest("GET", url, nil)
+  request.Header.Set("Authorization", "Bearer " + req.token)
+  resp, err := req.conn.Do(request)
+  if err != nil {
+    return err
+  }
+
+  var data *Entity
+
+  dec := json.NewDecoder(resp.Body)
+  err = dec.Decode(&data)
+  if err != nil {
+    return err
+  }
+  req.Data = data
+  log.Println("status", resp.StatusCode)
+
+  if resp.StatusCode == 403 {
+    return errors.New("Access Denied. You need an access token")
+  }
+
+  if resp.StatusCode == 401 {
+    return errors.New("Access Denied. Your access token is expired or invalid")
+  }
+
+  return nil
+}
+
+func NewThemeRequest (shopId, token string) (req *ThemeRequest) {
+  var t *Entity
+  conn := &http.Client{}
+  req = &ThemeRequest{shopId, token, t, conn}
+  return req
+}
+
+type ThemeStore struct {
+  dir string
+  theme *ThemeRequest
+}
+
+func (store *ThemeStore) writeTemplates() {
+  for _, template := range store.theme.Data.Entities["templates"] {
+    s := []string{template.Properties["name"], template.Properties["content_type"]}
+    fileName := strings.Join(s, ".")
+    dirAndFile := strings.Join([]string{store.dir, fileName}, "/")
+    log.Println(dirAndFile)
+    // remove file if exists
+    os.RemoveAll(dirAndFile)
+    // write file
+    err := ioutil.WriteFile(dirAndFile, []byte(template.Properties["body"]), 0644)
+    if err != nil {
+      log.Println("Could not write file", dirAndFile)
+    }
+  }
+}
+
+func (store *ThemeStore) writeAssets() {
+  for _, asset := range store.theme.Data.Entities["assets"] {
+    dirAndFile := strings.Join([]string{store.dir, "assets", asset.Properties["file_name"]}, "/")
+    log.Println(dirAndFile)
+  }
+}
+
+func (store *ThemeStore) Write() {
+  err := os.MkdirAll(strings.Join([]string{store.dir, "assets"}, "/"), 0700)
+  if err != nil {
+    log.Fatal("Could not write directories for shop " + store.dir)
+  }
+  store.writeTemplates()
+  store.writeAssets()
+}
+
+func NewThemeStore (dir string, theme *ThemeRequest) (store *ThemeStore) {
+  store = &ThemeStore{dir, theme}
+  return
+}
+
+func main () {
+  token := os.Getenv("BOOTIC_ACCESS_TOKEN")
+
+  if(token == "") {
+    log.Fatal("Please set the BOOTIC_ACCESS_TOKEN env variable")
+  }
+
+  theme := NewThemeRequest("29", token)
+  err := theme.Get()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  store := NewThemeStore("./" + theme.ShopId, theme)
+  store.Write()
+}
